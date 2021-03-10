c239da842c58e6881933604800fc9195141662d6
miniOrange <miniOrangeDev@users.noreply.github.com>
2021-03-10 14:54:49 +0530

    Update auth_mo_api.php

--- lang/en/auth_mo_api.php
+++ lang/en/auth_mo_api.php
@@ -20,25 +20,17 @@
  * Contains authentication method.
  *
  * @copyright   2020  miniOrange
- * @category    Authentication
  * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL v3 or later, see license.txt
  * @package     auth_mo_api
  */
 
-$string['mo_apititle'] = 'miniOrange API Authentication moodle';
-$string['mo_apidescription'] = '';
-$string['mo_api_create_or_update_warning'] = "When auto-provisioning or auto-update is enable,";
-$string['mo_api_empty_required_value'] = "is a required attribute, provide a valid value";
-$string['pluginname'] = 'miniOrange API Authentication';
-$string['pluginname_desc'] = 'abcd';
+$string['mo_apititle'] = 'Login using Moodle Users';
+$string['pluginname'] = 'Login using Moodle Users';
 $string['auth_mo_configure_api_setting'] = 'Moodle SSO solution by miniOrange allows your users to login to multiple applications via their Moodle credentials.This way, you can achieve Single Sign-On into your applications where the users will need to authenticate themselves via their Moodle credentials only once and they can access all the configured applications.';
 $string['mo_api_credentials'] = 'API Credentials: ';
 $string['mo_api_User_Authentication_API_URL'] = 'User Authentication API URL: ';
 $string['mo_api_apikey'] = 'API Key: ';
 $string['mo_api_authn_para'] = 'User Authentication Paramter: ';
-$string['mo_api_url_desc'] = 'Copy the default url';
-$string['mo_api_key_desc'] = 'Copy the API key';
-$string['mo_api_authn_para_desc'] = 'Copy the parameters';
 $string['mo_api_attributes'] = 'Attribute Release Statements';
 $string['mo_api_username'] = 'Username:';
 $string['mo_api_username_desc'] = 'Enter Username';
@@ -50,5 +42,4 @@ $string['mo_api_email'] = 'Email:';
 $string['mo_api_email_desc'] = 'Enter your email id';
 $string['mo_api_fullname'] = 'Full Name:';
 $string['mo_api_fullname_desc'] = 'Enter full name';
-$string['mo_api_button'] = 'Generate New Key';
-$string['mo_api_button_desc'] = 'abcd';
+
